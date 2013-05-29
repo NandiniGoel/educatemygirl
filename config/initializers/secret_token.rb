eda0921b6cdcf848bc3cafca83d8207a48562327
Nandini Goel <nandinigoel.india@gmail.com>
2013-05-29 14:40:25 +0530

    Added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Educatemygirl::Application.config.secret_token = '22137f3310f1337d5d452fa805a45da63bc09a922472547cd73ba19abfa4d923b58678fce0802140727cf4557947784c0e0961de69eb8e5a8b3f01dcb0403fb3'
